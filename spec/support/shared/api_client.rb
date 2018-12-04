357cda6501cc375b5fae63413c2febbb3fc9942b
dblock <dblock@dblock.org>
2018-12-04 12:30:02 -0500

    Replace access token when recording VCR cassettes. Part of #13.

--- spec/support/shared/api_client.rb
+++ spec/support/shared/api_client.rb
@@ -2,5 +2,5 @@ RSpec.shared_context 'API client', shared_context: :metadata do
   before do
     Strava::Api::Config.reset
   end
-  let(:client) { Strava::Api::Client.new(access_token: 'access-token') }
+  let(:client) { Strava::Api::Client.new(access_token: ENV['STRAVA_ACCESS_TOKEN'] || 'access-token') }
 end
--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -7,6 +7,7 @@ VCR.configure do |config|
   config.default_cassette_options = { record: :new_episodes }
   config.configure_rspec_metadata!
   config.before_record do |i|
+    i.request.headers['Authorization'] = ['Bearer access-token'] if ENV.key?('STRAVA_ACCESS_TOKEN')
     i.response.body.force_encoding('UTF-8')
   end
 end
