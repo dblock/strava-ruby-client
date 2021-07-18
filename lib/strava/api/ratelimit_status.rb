38dfd905a62ba0665897dea30924c568c33f51fb
Simon Neutert <simonneutert@users.noreply.github.com>
2021-07-18 17:56:57 +0200

    removes reliance on exceptions

--- lib/strava/api/ratelimit_status.rb
+++ lib/strava/api/ratelimit_status.rb
@@ -1,13 +1,13 @@
 module Strava
   module Api
-    class RatelimitStatus
+    class RatelimitHeaders
       attr_reader :headers
 
       def initialize(headers)
         @headers = headers
       end
 
-      def ratelimit_included?
+      def ratelimit?
         puts !!@headers['x-ratelimit-limit']
       end
 
@@ -15,44 +15,32 @@ module Strava
         @headers['x-ratelimit-limit']
       end
 
-      def ratelimit_limit_usage
+      def ratelimit_usage
         @headers['x-ratelimit-usage']
       end
 
       def ratelimit_fiveteen_minutes
-        ratelimit_limit.split(',').first.to_i
-      rescue NoMethodError
-        0
+        ratelimit? ? ratelimit_limit.split(',').first.to_i : 0
       end
 
       def ratelimit_total_day
-        ratelimit_limit.split(',').last.to_i
-      rescue NoMethodError
-        0
+        ratelimit? ? ratelimit_limit.split(',').last.to_i : 0
       end
 
       def ratelimit_fiveteen_minutes_usage
-        ratelimit_limit_usage.split(',').first.to_i
-      rescue NoMethodError
-        0
+        ratelimit? ? ratelimit_usage.split(',').first.to_i : 0
       end
 
       def ratelimit_total_day_usage
-        ratelimit_total_day_usage.split(',').last.to_i
-      rescue NoMethodError
-        0
+        ratelimit? ? ratelimit_total_day_usage.split(',').last.to_i : 0
       end
 
       def ratelimit_fiveteen_minutes_remaining
         ratelimit_fiveteen_minutes.to_i - ratelimit_fiveteen_minutes_usage.to_i
-      rescue NoMethodError
-        0
       end
 
       def ratelimit_total_day_remaining
         ratelimit_total_day.to_i - ratelimit_total_day_usage.to_i
-      rescue NoMethodError
-        0
       end
     end
   end
