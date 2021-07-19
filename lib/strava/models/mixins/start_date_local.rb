20d0807a126a2e5143f6a06a37e611f2933fb63c
Simon Neutert <simonneutert@users.noreply.github.com>
2021-07-19 16:45:20 +0200

    guard clause if property not existing

--- lib/strava/models/mixins/start_date_local.rb
+++ lib/strava/models/mixins/start_date_local.rb
@@ -9,17 +9,17 @@ module Strava
           def start_date_local
             dt = self['start_date_local']
 
-            shift = begin
-                if timezone.include?('+')
-                  "+0#{timezone.split('+').last.to_i}:00"
-                elsif timezone.include?('-')
-                  "-0#{timezone.split('-').last.to_i}:00"
-                end
-                    rescue NameError
-                      # formats time difference to hour
-                      "-0#{((start_date - dt) / 3600).to_i}:00"
-              end
+            # some strava object do not contain a timezone property i.e. `Lap`
+            unless try(:timeout)
+              shift = "-0#{((start_date - dt) / 3600).to_i}:00"
+              return ::Time.new(dt.year, dt.month, dt.day, dt.hour, dt.min, dt.sec, shift)
+            end
 
+            if timezone.include?('+')
+              "+0#{timezone.split('+').last.to_i}:00"
+            elsif timezone.include?('-')
+              "-0#{timezone.split('-').last.to_i}:00"
+            end
             ::Time.new(dt.year, dt.month, dt.day, dt.hour, dt.min, dt.sec, shift)
           end
         end
