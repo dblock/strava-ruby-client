e8a912fb8c4bc4f98566d4348fca308312e3a7a8
Simon Neutert <simonneutert@users.noreply.github.com>
2021-12-26 12:53:46 +0100

    let's see where this gets us

--- lib/strava/models/mixins/start_date_local.rb
+++ lib/strava/models/mixins/start_date_local.rb
@@ -6,21 +6,54 @@ module Strava
 
         included do
           property 'start_date_local', transform_with: ->(v) { ::Time.parse(v) }
+
           def start_date_local
             dt = self['start_date_local']
-
             # some strava object do not contain a timezone property i.e. `Lap`
-            unless try(:timeout)
-              shift = "-0#{((start_date - dt) / 3600).to_i}:00"
-              return ::Time.new(dt.year, dt.month, dt.day, dt.hour, dt.min, dt.sec, shift)
-            end
+            timezone_shift = conditional_timezone(dt)
+            ::Time.new(dt.year, dt.month, dt.day, dt.hour, dt.min, dt.sec, timezone_shift)
+          end
 
-            if timezone.include?('+')
-              "+0#{timezone.split('+').last.to_i}:00"
-            elsif timezone.include?('-')
-              "-0#{timezone.split('-').last.to_i}:00"
+          private
+
+          def conditional_timezone(dt)
+            if key?(:timezone)
+              if timezone.include?('+')
+                timezone_shift_string('+')
+              elsif timezone.include?('-')
+                timezone_shift_string('-')
+              else
+                raise ArgumentError 'No operator of timezone correction detectable!'
+              end
+            else
+              calculate_timezone(dt)
             end
-            ::Time.new(dt.year, dt.month, dt.day, dt.hour, dt.min, dt.sec, shift)
+          end
+
+          def calculate_timezone(dt)
+            if start_date == dt
+              timezone_diff_shift_string((dt - dt), '-')
+            elsif dt < start_date
+              timezone_diff_shift_string((dt - start_date), '-')
+            elsif dt > start_date
+              timezone_diff_shift_string((dt - start_date), '+')
+            else
+              raise ArgumentError 'No operator of timezone correction detectable!'
+            end
+          end
+
+          def timezone_shift_string(operator)
+            diff_hours = timezone.split(operator).last.to_i
+            "#{operator}#{format_int_leading_zero(diff_hours)}:00"
+          end
+
+          def timezone_diff_shift_string(time_diff, operator)
+            diff_hours = (time_diff.abs / 3600).to_i
+            "#{operator}#{format_int_leading_zero(diff_hours)}:00"
+          end
+
+          def format_int_leading_zero(int)
+            '%02d' % int
           end
         end
       end
