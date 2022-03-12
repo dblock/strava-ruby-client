0d55e4ff27bca606597d69cfe58c17cd7b2c04dc
Simon Neutert <simonneutert@users.noreply.github.com>
2022-03-12 19:06:31 +0100

    adds rack env for active support bug

--- lib/strava-ruby-client.rb
+++ lib/strava-ruby-client.rb
@@ -5,6 +5,7 @@ require 'logger'
 require 'hashie'
 require 'time'
 
+require 'active_support' if ENV['RACK_ENV'] == 'test'
 require 'active_support/core_ext/object/to_query'
 require 'active_support/core_ext/hash'
 require 'active_support/concern'
