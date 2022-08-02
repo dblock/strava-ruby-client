80f559fea85ff227b87b605d1530a18c3bf7595a
simonneutert <simonneutert@users.noreply.github.com>
2022-08-02 18:40:17 +0200

    adds cassettes for activity comments, kudos, photos

--- spec/strava/api/client/endpoints/activities/activity_kudos_spec.rb
+++ spec/strava/api/client/endpoints/activities/activity_kudos_spec.rb
@@ -5,17 +5,17 @@ require 'spec_helper'
 RSpec.describe 'Strava::Api::Client#activity_kudos', vcr: { cassette_name: 'client/activity_kudos' } do
   include_context 'API client'
   it 'returns activity kodoers' do
-    activity_kudos = client.activity_kudos(id: 1_946_417_534)
+    activity_kudos = client.activity_kudos(id: 3_958_491_750)
     expect(activity_kudos).to be_a Enumerable
-    expect(activity_kudos.count).to eq 33
+    expect(activity_kudos.count).to eq 70
     activity_kudoer = activity_kudos.first
     expect(activity_kudoer).to be_a Strava::Models::Athlete
-    expect(activity_kudoer.id).to eq 175_275
-    expect(activity_kudoer.username).to eq 'fberselius'
+    expect(activity_kudoer.id).to eq nil # strava cuts it off due to user privacy
+    expect(activity_kudoer.username).to be nil
   end
   it 'returns activity kodoers by id' do
-    activity_kudos = client.activity_kudos(1_946_417_534)
+    activity_kudos = client.activity_kudos(3_958_491_750)
     expect(activity_kudos).to be_a Enumerable
-    expect(activity_kudos.count).to eq 33
+    expect(activity_kudos.count).to eq 70
   end
 end
--- spec/strava/api/client/endpoints/activities/activity_photos_spec.rb
+++ spec/strava/api/client/endpoints/activities/activity_photos_spec.rb
@@ -5,28 +5,28 @@ require 'spec_helper'
 RSpec.describe 'Strava::Api::Client#activity_photos', vcr: { cassette_name: 'client/activity_photos' } do
   include_context 'API client'
   it 'returns activity photos' do
-    activity_photos = client.activity_photos(id: 1_946_417_534)
+    activity_photos = client.activity_photos(id: 3_958_491_750)
     expect(activity_photos).to be_a Enumerable
-    expect(activity_photos.count).to eq 9
+    expect(activity_photos.count).to eq 1
     activity_photo = activity_photos.first
     expect(activity_photo.id).to eq nil
-    expect(activity_photo.unique_id).to eq '65889142-538D-4EE5-96F5-3DC3B773B1E3'
-    expect(activity_photo.urls).to eq('0' => 'https://dgtzuqphqg23d.cloudfront.net/eb4DMJ2hJW3k_g9URZEMfaJ8rZfHagrNlZRuEZz0osU-29x64.jpg')
+    expect(activity_photo.unique_id).to eq 'F775717B-D1C1-443A-AD99-3D9A80FF11C9'
+    expect(activity_photo.urls).to eq('1800' => 'https://d3nn82uaxijpm6.cloudfront.net/assets/media/placeholder-photo@4x-13b0b44cfa828acc8b95d8dc4b8157d87666aa1ea8ef814c6ec36cd542d2b756.png')
     expect(activity_photo.source).to eq 1
-    expect(activity_photo.athlete_id).to eq 26_462_176
-    expect(activity_photo.activity_id).to eq 1_946_417_534
-    expect(activity_photo.activity_name).to eq 'TCS NYC Marathon 2018'
+    expect(activity_photo.athlete_id).to eq 24_776_507
+    expect(activity_photo.activity_id).to eq 3_958_491_750
+    expect(activity_photo.activity_name).to eq 'Bitche, please 🥳'
     expect(activity_photo.resource_state).to eq 2
     expect(activity_photo.caption).to eq ''
     expect(activity_photo.created_at).to be_a Time
     expect(activity_photo.created_at_local).to be_a Time
     expect(activity_photo.uploaded_at).to be_a Time
-    expect(activity_photo.sizes).to eq('0' => [29, 64])
-    expect(activity_photo.default_photo).to be false
+    expect(activity_photo.sizes).to eq('1800' => [1372, 1000])
+    expect(activity_photo.default_photo).to be true
   end
   it 'returns activity photos by id' do
-    activity_photos = client.activity_photos(1_946_417_534)
+    activity_photos = client.activity_photos(3_958_491_750)
     expect(activity_photos).to be_a Enumerable
-    expect(activity_photos.count).to eq 9
+    expect(activity_photos.count).to eq 1
   end
 end
