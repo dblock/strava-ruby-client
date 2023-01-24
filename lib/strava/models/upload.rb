32e45a110c706cbb9da66f8f62783e8f4b17d83e
Simon Neutert <simonneutert@users.noreply.github.com>
2023-01-24 17:06:51 +0100

    fixes #22

--- lib/strava/models/upload.rb
+++ lib/strava/models/upload.rb
@@ -8,6 +8,40 @@ module Strava
       property 'error'
       property 'status'
       property 'activity_id'
+
+      def processing?
+        validate!
+        activity_id.nil?
+      end
+
+      def processed?
+        !processing?
+      end
+
+      private
+
+      def validate!
+        raise_when_background_job_failed!
+      end
+
+      def raise_when_background_job_failed!
+        response = self['http_response'].response
+        return unless response_contains_error_message?(response)
+
+        raise Strava::Errors::UploadError, response_values(response)
+      end
+
+      def response_contains_error_message?(response)
+        response.status == 200 && response.body['error'].present?
+      end
+
+      def response_values(response)
+        {
+          status: response.status,
+          headers: response.headers,
+          body: response.body
+        }
+      end
     end
   end
 end
