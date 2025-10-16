a117c5b566eee7a6a811d56a9372d377b116002a
Daniel (dB.) Doubrovkine <dblock@dblock.org>
2025-10-16 10:01:09 -0400

    Respect Faraday::Response::RaiseError::DEFAULT_OPTIONS when raising errors.

--- lib/strava/errors/ratelimit_error.rb
+++ lib/strava/errors/ratelimit_error.rb
@@ -3,29 +3,11 @@
 module Strava
   module Errors
     class RatelimitError < ::Faraday::ClientError
-      attr_reader :http_response, :ratelimit, :error_message
+      attr_reader :ratelimit
 
-      def initialize(http_response, error_message = nil)
-        @response = http_response.response
-        @ratelimit = Strava::Api::Ratelimit.new(@response)
-        @error_message = error_message || message
-        super({
-          status: http_response.status,
-          headers: http_response.response_headers,
-          body: http_response.body
-        })
-      end
-
-      def message
-        response[:body]['message'] || super
-      end
-
-      def headers
-        response[:headers]
-      end
-
-      def errors
-        response[:body]['errors']
+      def initialize(env, response)
+        @ratelimit = Strava::Api::Ratelimit.new(env.response)
+        super(response)
       end
     end
   end
--- spec/strava/api/client_spec.rb
+++ spec/strava/api/client_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe Strava::Api::Client do
   end
 
   it_behaves_like 'web client'
+
   context 'with errors' do
     it 'handles authorization errors', vcr: { cassette_name: 'client/authorization_error' } do
       expect { client.activity(id: 1_946_417_534) }.to raise_error Strava::Errors::Fault, /Authorization Error/
@@ -17,6 +18,53 @@ RSpec.describe Strava::Api::Client do
 
     it 'handles not found errors', vcr: { cassette_name: 'client/not_found_error' } do
       expect { client.activity(id: 1_946_417_534) }.to raise_error Faraday::ResourceNotFound
+    end
+
+    it 'includes method url and url_path', vcr: { cassette_name: 'client/not_found_error' } do
+      expect { client.activity(id: 1_946_417_534) }.to raise_error Faraday::ResourceNotFound do |error|
+        request = error.response[:request]
+        expect(request[:method]).to eq :get
+        expect(request[:url]).to eq URI('https://www.strava.com/api/v3/activities/1946417534')
+        expect(request[:url_path]).to eq '/api/v3/activities/1946417534'
+      end
+    end
+
+    context 'with DEFAULT_OPTIONS[:include_request] set to false' do
+      before do
+        allow_any_instance_of(Strava::Web::RaiseResponseError).to receive(:options)
+          .and_return({ include_request: false, allowed_statuses: [] })
+      end
+
+      it 'does not include method url and url_path in Faraday::ResourceNotFound', vcr: { cassette_name: 'client/not_found_error' } do
+        expect { client.activity(id: 1_946_417_534) }.to raise_error Faraday::ResourceNotFound do |error|
+          expect(error.response[:request]).to be_nil
+        end
+      end
+
+      it 'does not include method url and url_path in Strava::Errors::RatelimitError', vcr: { cassette_name: 'client/activity_with_ratelimit_exceeded' } do
+        expect do
+          client.activity(1_946_417_534)
+        end.to raise_error(Strava::Errors::RatelimitError) do |error|
+          expect(error.ratelimit).to be_a Strava::Api::Ratelimit
+          expect(error.ratelimit.exceeded?).to be(true)
+          expect(error.ratelimit.exceeded).to eql({ fifteen_minutes_remaining: 0 })
+          expect(error.response[:request]).to be_nil
+        end
+      end
+    end
+
+    it 'raises Strava::Errors::RatelimitError', vcr: { cassette_name: 'client/activity_with_ratelimit_exceeded' } do
+      expect do
+        client.activity(1_946_417_534)
+      end.to raise_error(Strava::Errors::RatelimitError) do |error|
+        expect(error.ratelimit).to be_a Strava::Api::Ratelimit
+        expect(error.ratelimit.exceeded?).to be(true)
+        expect(error.ratelimit.exceeded).to eql({ fifteen_minutes_remaining: 0 })
+        request = error.response[:request]
+        expect(request[:method]).to eq :get
+        expect(request[:url]).to eq URI('https://www.strava.com/api/v3/activities/1946417534')
+        expect(request[:url_path]).to eq '/api/v3/activities/1946417534'
+      end
     end
   end
 
