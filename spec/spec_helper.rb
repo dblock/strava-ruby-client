85b501d9a93be69a57a2bfcea84760d2f5231a29
Daniel (dB.) Doubrovkine <dblock@dblock.org>
2025-10-22 11:51:13 -0400

    Parse route with tcxread.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,10 @@ require 'rubygems'
 require 'rspec'
 require 'strava-ruby-client'
 
+require 'tempfile'
 require 'multi_xml'
 require 'gpx'
+require 'tcxread'
 require 'polylines'
 
 Dir[File.join(File.dirname(__FILE__), 'support', '**/*.rb')].each do |file|
--- spec/strava/api/client/endpoints/routes/export_route_tcx_spec.rb
+++ spec/strava/api/client/endpoints/routes/export_route_tcx_spec.rb
@@ -4,12 +4,38 @@ require 'spec_helper'
 
 RSpec.describe 'Strava::Api::Client#export_route_tcx', vcr: { cassette_name: 'client/export_route_tcx' } do
   include_context 'with API client'
-  it 'exports a route file' do
-    route = client.export_route_tcx(id: 16_341_573)
-    expect(route).to start_with "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<TrainingCenterDatabase"
 
-    xml = MultiXml.parse(route)
-    expect(xml).to be_a Hash
+  context 'an exported route' do
+    let(:route) { client.export_route_tcx(id: 16_341_573) }
+
+    it 'is a TrainingCenterDatabase' do
+      expect(route).to start_with "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<TrainingCenterDatabase"
+    end
+
+    context 'as xml' do
+      let(:xml) { MultiXml.parse(route) }
+
+      it 'is valid XML' do
+        expect(xml).to be_a Hash
+      end
+    end
+
+    context 'as parsed tcx' do
+      let(:tcx) do
+        Tempfile.open(['route', '.tcx']) do |temp_file|
+          temp_file.write(route)
+          temp_file.close
+
+          TCXRead.new(temp_file.path)
+        end
+      end
+
+      it 'is valid' do
+        expect(tcx).to be_a(TCXRead)
+        expect(tcx.total_distance_meters).to eq 0
+        expect(tcx.total_time_seconds).to eq 0
+      end
+    end
   end
 
   it 'exports a route file by id' do
